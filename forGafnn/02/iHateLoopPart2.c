806761186115d3948e51c6f1618aa28041fabbef
Krissada Sarawit <topfeekrissada@outlook.com>
2021-02-19 18:56:25 +0700

    iHateForLoopPart2.c

---
+++ forGafnn/02/iHateLoopPart2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void printTriangle(long);
+
+void run()
+{
+    long input;
+    printf("Enter : ");
+    scanf("%ld", &input);
+    printTriagnle(input);
+}
+
+void printTriagnle(long num)
+{
+    /*First Half*/
+    for(int i = 0 ; i < num/2 ; i++)
+    {
+        /* in line print*/
+        for(int k = 0 ; k <= i ; k++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    /*Second Half */
+    for(int i = (num/2) +1; i>0; i--) 
+    {
+        /* in line print */
+        for(int k = 1; k <= i ; k++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+}
+
+
+
+
+
+
+
+
+int main ()
+{
+    run();
+    return 0;
+}
